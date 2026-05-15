6ddb78a03fed166d6a82a2e878954638b6bae088
ellavisionmedi3-cyber <ellavisionmedi3@gmail.com>
2026-05-15 05:53:45 +0300

    Upload next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  serverExternalPackages: ["better-sqlite3"],
+  turbopack: {},
+};
+
+export default nextConfig;
